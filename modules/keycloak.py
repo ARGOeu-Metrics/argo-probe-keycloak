6f3547851b34f0463816ad4e148552830fcf0a03
Katarina Zailac <katarina.zailac@gmail.com>
2024-02-05 10:00:57 +0100

    Separate main function from module

--- modules/keycloak.py
+++ modules/keycloak.py
@@ -1,32 +1,8 @@
-import argparse
-import sys
-
 import requests
 from argo_probe_keycloak.status import Status
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description='Nagios probe for Keycloak login'
-    )
-    parser.add_argument(
-        "--token_endpoint", dest="endpoint", type=str, required=True,
-        help="The token endpoint"
-    )
-    parser.add_argument(
-        "--client_id", dest="client_id", type=str, required=True,
-        help="The identifier of the client"
-    )
-    parser.add_argument(
-        "--client_secret", dest="client_secret", type=str, required=True,
-        help="The secret value of the client"
-    )
-    parser.add_argument(
-        "-t", "--timeout", dest="timeout", type=int, default=60,
-        help="timeout"
-    )
-    args = parser.parse_args()
-
+def fetch_keycloak_token(args):
     status = Status()
 
     try:
@@ -57,9 +33,7 @@ def main():
     ) as e:
         status.set_critical(str(e))
 
-    print(status.get_message())
-    sys.exit(status.get_code())
-
-
-if __name__ == '__main__':
-    main()
+    return {
+        "message": status.get_message(),
+        "code": status.get_code()
+    }
